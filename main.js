480aabe19c208bbf183769d5cbdb7fad19fb97b6
alexniggemeyer <alex.niggemeyer@gmail.com>
2020-04-11 08:30:55 +0200

    adding dots and fixing margins

--- main.js
+++ main.js
@@ -3,28 +3,33 @@ var projects = document.querySelector('.projects');
 var projectWidth = document.querySelector('.project').offsetWidth;
 var projectsWidth = document.querySelector('.projects').offsetWidth;
 var project = document.querySelectorAll('.project');
+var dot = document.querySelectorAll('.dot');
 
-projectCount = project.length;
-
-console.log(projectWidth);
-console.log('translate(' + (-2* projectsWidth) + 'px)');
+let projectCount = project.length;
+let projectMargins = parseFloat(window.getComputedStyle(project[0]).marginLeft) + parseFloat(window.getComputedStyle(project[0]).marginRight);
+console.log(projectMargins);
 
 function setTransform(){
     projects.style.transform = 'translate(' + (0.5*projectsWidth - (pos*projectWidth)) + 'px)';
+    dot[pos].classList.add('active');
 }
 
 function next(){
+    dot[pos].classList.remove('active');
     pos = pos + 1;
     setTransform();
     console.log(pos);
 }
 
 function back(){
+    dot[pos].classList.remove('active');
     pos = pos - 1;
     setTransform();
     console.log(pos);
 }
 
 setTransform();
+
+
 
 window.addEventListener('resize', setTransform);
