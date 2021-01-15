eb8f1444e878ce621a0473d877f0a0235df31610
alex n <alex.niggemeyer@gmail.com>
2021-01-15 15:15:05 +0100

    Fridaaaaaaay

--- projects/artificialUser/artificialUser.js
+++ projects/artificialUser/artificialUser.js
@@ -3,13 +3,34 @@ window.addEventListener('scroll', ()=>{
     document.querySelector('.move').style.top = scroll /-2.5 + 'px';
 })
 
+function remap(input,min,max,newMin,newMax){
 
-let headingWidth = (window.innerWidth/16)*9;
+    output = newMin + ((input-min)/(max-min))*(newMax-newMin);
+
+    if(output > newMax){
+        output = newMax;
+    };
+
+    return output;
+}
 
 
-if(window.innerWidth >= 600){
-    document.querySelector("#intro").setAttribute("style", "height:" + headingWidth + "px;");
-    console.log("HIH");
+function headerSizing(){
+
+    let headingWidth = (window.innerWidth/16)*9;
+
+    if(window.innerWidth >= 600){
+        document.querySelector("#intro").setAttribute("style", "height:" + headingWidth + "px;");
+        
+
+        let input = window.innerWidth
+        document.querySelector(".heading").setAttribute("style", "transform:  scale(" + remap(input,600,1920,1,2) + ");");
+        
+    };
 };
 
+
+window.addEventListener('resize', headerSizing);
+
+headerSizing();
 
