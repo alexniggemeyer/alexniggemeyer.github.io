e7328cc0f67e930beb75459502ec21df06644751
alex n <alex.niggemeyer@gmail.com>
2021-02-03 12:42:44 +0100

    first content for ik

--- projects/projectFunctions.js
+++ projects/projectFunctions.js
@@ -29,19 +29,19 @@ headerSizing();
 
 window.addEventListener('resize', headerSizing);
 
-// Popuphandler
+// Popuphandler for AU Screenshot
 
+let grid = document.querySelector(".grid");
 let gridItem = document.querySelectorAll('.gridItem');
 let popup = document.querySelector('.popup');
 let Arrow = document.createElement("style");
 document.head.appendChild(Arrow);
 
 
+
 gridItem.forEach(element => {
     
     element.addEventListener('mouseover', (e)=>{
-        
-        console.log(element.offsetHeight);
 
         if(element.className == "gridItem prototype"){
             popup.textContent = "Import the prototype!"
@@ -56,16 +56,68 @@ gridItem.forEach(element => {
             popup.textContent = "The Log Window outputs interactions of the artificial User with the prototype, to give detailed feedback."
         }
 
-        let popupWidth = popup.offsetWidth;
+        if(window.innerWidth > 600){
 
-        if(element.offsetLeft < 330){
-            Arrow.innerHTML = ".popup:before{ content: ''; position: absolute; border-width: 1rem; top: 1rem; margin-left: -3rem; border-style: solid; border-color:  transparent #39B4CF transparent transparent;}";
-            popup.setAttribute("style", `display: inline; transform: translate(${element.offsetLeft+element.offsetWidth+16}px, ${element.offsetTop + (element.offsetHeight/4)}px)`);
-            
+            //for desktop
+
+            let popupWidth = popup.offsetWidth;
+
+            if(element.offsetLeft < 330){
+                Arrow.innerHTML = ".popup:before{ content: ''; position: absolute; border-width: 1rem; top: 1rem; margin-left: -3rem; border-style: solid; border-color:  transparent #39B4CF transparent transparent;}";
+                popup.setAttribute("style", `display: inline;max-width: 10rem; transform: translate(${element.offsetLeft+element.offsetWidth+16}px, ${element.offsetTop + (element.offsetHeight/4)}px)`);
+                
+            }else{
+                Arrow.innerHTML = ".popup:after{ content: ''; position: absolute; border-width: 1rem; top: 1rem; margin-left: 50%; left: 50%;border-style: solid; border-color:  transparent transparent transparent  #39B4CF;}";
+                popup.setAttribute("style", `display: inline;max-width: 10rem; transform: translate(${element.offsetLeft -popupWidth -16}px, ${element.offsetTop + (element.offsetHeight/4)}px)`);
+            };
         }else{
-            Arrow.innerHTML = ".popup:after{ content: ''; position: absolute; border-width: 1rem; top: 1rem; margin-left: 50%; left: 50%;border-style: solid; border-color:  transparent transparent transparent  #39B4CF;}";
-            popup.setAttribute("style", `display: inline; transform: translate(${element.offsetLeft -popupWidth -16}px, ${element.offsetTop + (element.offsetHeight/4)}px)`);
-        };
+
+            //for mobile
+
+            popupWidth = grid.offsetWidth;
+            popup.setAttribute("style", `display: inline;width:${popupWidth-32}px;  transform: translate(${grid.offsetLeft-16}px, ${grid.offsetHeight+16}px)`);
+            
+            if(element.className == "gridItem userSettings" || "gridItem prototype"){
+                Arrow.innerHTML = ".popup:after{ content: ''; position: absolute; border-width: 1rem; top:-1.95rem; left: 10%;border-style: solid; border-color:  transparent transparent #39B4CF transparent;}";
+            }
+            if(element.className == "gridItem preview"){
+                Arrow.innerHTML = ".popup:after{ content: ''; position: absolute; border-width: 1rem; top:-1.95rem; left: 50%;border-style: solid; border-color:  transparent transparent #39B4CF transparent;}";
+            }
+            if(element.className == "gridItem log"){
+                Arrow.innerHTML = ".popup:after{ content: ''; position: absolute; border-width: 1rem; top:-1.95rem; left: 85%;border-style: solid; border-color:  transparent transparent #39B4CF transparent;}";
+            }
+        
+        }
+
+
+
+
     });
+});
+
+
+//IK Challange Animations
+
+
+let counterText = document.querySelector(".counter");
+let counter = 0;
+counterText.textContent = counter;
+
+
+window.addEventListener('scroll', (e)=>{
+    console.log(counterText.getBoundingClientRect().top);
+    if(counterText.getBoundingClientRect().top < 700){
+        let test = setInterval(() => {
+            if(counter < 575){
+                counter += 1;
+                counterText.textContent = counter;        
+            }
+        
+            
+        }, 1);
+    }
+    
+
+    
 });
 
